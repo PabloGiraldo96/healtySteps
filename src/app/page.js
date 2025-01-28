a322704e058b79bbffed43a4df96a10f34740eec
emptybrother7 <emptybrother7@gmail.com>
2025-01-28 11:58:08 +0000

    Head + Title + Meta on Page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,26 @@
 "use client";
 
+import Head from 'next/head'; 
 import StepCounter from '@/components/StatsUI';
 import ThemeToggle from '@/components/ThemeToggle';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
-      <header className="p-4 flex justify-end">
-        <ThemeToggle />
-      </header>
-      <main className="text-center p-4">
-        <h1 className="text-2xl font-bold mb-4">Welcome to WateRPG</h1>
-        <StepCounter />
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>💧 WateRPG 💧</title>
+        <meta name="description" content="Welcome to WateRPG - A new text RPG!" />
+      </Head>
+
+      <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
+        <header className="p-4 flex justify-end">
+          <ThemeToggle />
+        </header>
+        <main className="text-center p-4">
+          <h1 className="text-2xl font-bold mb-4">Welcome to WateRPG</h1>
+          <StepCounter />
+        </main>
+      </div>
+    </>
   );
 }
