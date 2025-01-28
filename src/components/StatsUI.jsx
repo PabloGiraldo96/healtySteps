762deaf30759a2bf594b681036758fd1ad66b2af
emptybrother7 <emptybrother7@gmail.com>
2025-01-28 14:00:44 +0000

    rearrange StatUI stats

--- src/components/StatsUI.jsx
+++ src/components/StatsUI.jsx
@@ -166,15 +166,15 @@ const StatsUI = () => {
           <div className="p-4 bg-gray-100 dark:bg-gray-800 rounded-lg">
             <p className="text-lg font-semibold">Level: {level}</p>
             <p className="text-lg font-semibold">Gold: </p>
-            <p className="text-lg font-semibold">Stat Points: {statPoints}</p>
-            <p className="text-lg font-semibold">Strength: {strength}</p>
-            <p className="text-lg font-semibold">Intelligence: {intelligence}</p>
-            <p className="text-lg font-semibold">Agility: {agility}</p>
-            <p className="text-lg font-semibold">Endurance: {endurance}</p>
             <h2 className="text-xl font-semibold">Steps</h2>
             <p className="text-lg">{steps} Traveled</p>
             <h2 className="text-xl font-semibold">Water Intake</h2>
             <p className="text-lg">{waterIntake} Water</p>
+            <p className="text-lg font-semibold">Strength: {strength}</p>
+            <p className="text-lg font-semibold">Intelligence: {intelligence}</p>
+            <p className="text-lg font-semibold">Agility: {agility}</p>
+            <p className="text-lg font-semibold">Endurance: {endurance}</p>
+            <p className="text-lg font-semibold">Available Stat Points: {statPoints}</p>
           </div>
           <div className="grid grid-cols-3 gap-3">
             <button
