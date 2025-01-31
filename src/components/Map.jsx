5bd5d2d6d014bcc074eaa43e05e33532608fe6cb
emptybrother7 <emptybrother7@gmail.com>
2025-01-31 01:57:01 +0000

    Got rid of alert prompting 2 times when moving locations

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,35 +1,23 @@
 "use client";
 
-import { useState } from 'react';
-
 const Map = ({ onLocationSelect }) => {
-  const [currentLocation, setCurrentLocation] = useState("Home");
-
-  const handleTravel = (location) => {
-    const confirmTravel = window.confirm(`Do you want to travel to ${location}?`);
-    if (confirmTravel) {
-      setCurrentLocation(location);
-      onLocationSelect(location); 
-    }
-  };
-
   return (
     <div className="space-y-4 flex flex-col place-items-center">
       <h2 className="text-2xl font-semibold text-center mb-4">Map</h2>
       <button
-        onClick={() => handleTravel("Watwon")}
+        onClick={() => onLocationSelect("Watwon")}
         className="w-1/6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
       >
         Watwon
       </button>
       <button
-        onClick={() => handleTravel("Wishing Well")}
+        onClick={() => onLocationSelect("Wishing Well")}
         className="w-1/6 px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600"
       >
         Wishing Well
       </button>
       <button
-        onClick={() => handleTravel("Home")}
+        onClick={() => onLocationSelect("Home")}
         className="w-1/6 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
       >
         Home
