d4351f1c1dddd2cfe1d7af324f7a661cd5b22dbe
emptybrother7 <emptybrother7@gmail.com>
2025-01-31 23:21:49 +0000

    fixed inventory button

--- src/components/MainUI.jsx
+++ src/components/MainUI.jsx
@@ -151,7 +151,7 @@ const MainUI = () => {
           <Inventory waterIntake={waterIntake} />
           <button
             onClick={() => setShowInventory(false)}
-            className="w-full px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+            className="w-1/6 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
           >
             Back to Main Menu
           </button>
