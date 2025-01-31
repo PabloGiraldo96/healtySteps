14762137aa7e34432f63c1614f132476cce6658e
emptybrother7 <emptybrother7@gmail.com>
2025-01-31 23:12:17 +0000

    Fixed explore button sizes

--- src/components/ExploreMenu.jsx
+++ src/components/ExploreMenu.jsx
@@ -3,15 +3,15 @@ const ExploreMenu = ({ currentLocation, onBack }) => {
     <div className="space-y-4 flex flex-col place-items-center">
       {currentLocation === "Home" && (
         <>
-          <button className="w-3/6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+          <button className="w-1/6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
             Storage
           </button>
-          <button className="w-3/6 px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600">
+          <button className="w-1/6 px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600">
             Sleep
           </button>
           <button
             onClick={onBack}
-            className="w-3/6 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+            className="w-1/6 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
           >
             Back
           </button>
@@ -37,25 +37,25 @@ const ExploreMenu = ({ currentLocation, onBack }) => {
         <>
           <button
             onClick={() => alert("Store clicked!")}
-            className="w-3/6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+            className="w-1/6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
           >
             Store
           </button>
           <button
             onClick={() => alert("Bar clicked!")}
-            className="w-3/6 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+            className="w-1/6 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
           >
             Bar
           </button>
           <button
             onClick={() => alert("Church clicked!")}
-            className="w-3/6 px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600"
+            className="w-1/6 px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600"
           >
             Church
           </button>
           <button
             onClick={onBack}
-            className="w-3/6 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+            className="w-1/6 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
           >
             Back
           </button>
